f9b6ab0543f0694d7389438f8891c273355e4e3b
bhrtgbrt <chengandrew0513@gmail.com>
2024-11-29 14:50:17 +0800

    1129

---
+++ Lab12/app/src/main/java/com/example/lab12/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.lab12
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        findViewById<Button>(R.id.btnStart).setOnClickListener {
+            startService(Intent(this, MyService::class.java))
+            Toast.makeText(
+                this, "啟動Service", Toast.LENGTH_SHORT
+            ).show()
+            finish()
+        }
+    }
+}
